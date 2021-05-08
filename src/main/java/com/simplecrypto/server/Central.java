a7eb2ba2f73d0efebe3f5a2bd4119d0f5e05c022
Mirko de Cillis <decillis.web@gmail.com>
2021-05-08 13:18:55 +0200

    first commit

---
+++ src/main/java/com/simplecrypto/server/Central.java
@@ -0,0 +1,13 @@
+package com.simplecrypto.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Central {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Central.class, args);
+    }
+
+}
